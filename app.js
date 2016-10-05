5641e181c81f12120df42558a2ca1681ec1b830d
Clément Blanco <clement.blanco@gmail.com>
2016-10-05 14:07:39 +0100

    Transformed into Standard Javascript

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
 'use strict'
 
-var fs = require('fs')
-var get = require('simple-get')
-var cloudinary = require('cloudinary')
-var runLimit = require('run-parallel-limit')
-var mkdirp = require('mkdirp')
+var fs = require('fs');
+var get = require('simple-get');
+var cloudinary = require('cloudinary');
+var runLimit = require('run-parallel-limit');
+var mkdirp = require('mkdirp');
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.SECRET_KEY
-})
+});
 
 cloudinary.api.resources(function (result) {
-  let urls = result.resources.map(img => {
+  var urls = result.resources.map(function (img) {
     return img.url
-  })
+  });
 
-  downloadImages(urls)
+  downloadImages(urls);
 }, {type: 'upload', max_results: '500'})
 
 function downloadImages (urls) {
   mkdirp('./images', function (err) {
-    if (err) return console.error('Error creating images directory', err)
+    if (err) return console.error('Error creating images directory', err);
 
     runLimit(urls.map(function (url) {
       return function (callback) {
-        let filename = url.split('/')[url.split('/').length - 1]
+        var filename = url.split('/')[url.split('/').length - 1];
 
         get(url, function (err, res) {
-          if (err) return console.error('error requesting', err)
+          if (err) return console.error('error requesting', err);
 
-          var stream = res.pipe(fs.createWriteStream(`./images/${filename}`, {defaultEncoding: 'binary'}))
+          var stream = res.pipe(fs.createWriteStream('./images/'+filename, {defaultEncoding: 'binary'}));
 
           stream.on('finish', function () {
-            console.log(`Added => ${filename}`)
-            return callback(null, url)
-          })
-        })
+            console.log('Added => '+filename);
+            return callback(null, url);
+          });
+        });
       }
     }), 10, function (err) {
-      if (err) return console.error(err)
-      console.log(`Done! All images written to ${__dirname}/images`)
-    })
-  })
+      if (err) return console.error(err);
+      console.log('Done! All images written to ./images');
+    });
+  });
 }
