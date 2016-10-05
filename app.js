b46ea9531c7fc4914514139963a286c627c42842
Clément Blanco <clement.blanco@gmail.com>
2016-10-05 14:32:16 +0100

    Adding `next_cursor` support.

--- app.js
+++ app.js
@@ -7,41 +7,57 @@ var runLimit = require('run-parallel-limit');
 var mkdirp = require('mkdirp');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.SECRET_KEY
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.SECRET_KEY
 });
 
-cloudinary.api.resources(function (result) {
-  var urls = result.resources.map(function (img) {
-    return img.url
-  });
+function downloadImages (urls, callback)
+{
+    callback = callback || function(){};
 
-  downloadImages(urls);
-}, {type: 'upload', max_results: '500'})
+    mkdirp('./images', function (err) {
+        if (err) return console.error('Error creating images directory', err);
 
-function downloadImages (urls) {
-  mkdirp('./images', function (err) {
-    if (err) return console.error('Error creating images directory', err);
+        runLimit(urls.map(function (url) {
+            return function (callback) {
+                var filename = url.split('/')[url.split('/').length - 1];
 
-    runLimit(urls.map(function (url) {
-      return function (callback) {
-        var filename = url.split('/')[url.split('/').length - 1];
+                get(url, function (err, res) {
+                    if (err) return console.error('error requesting', err);
 
-        get(url, function (err, res) {
-          if (err) return console.error('error requesting', err);
+                    var stream = res.pipe(fs.createWriteStream('./images/'+filename, {defaultEncoding: 'binary'}));
 
-          var stream = res.pipe(fs.createWriteStream('./images/'+filename, {defaultEncoding: 'binary'}));
-
-          stream.on('finish', function () {
-            console.log('Added => '+filename);
-            return callback(null, url);
-          });
+                    stream.on('finish', function () {
+                        console.log('Added => '+filename);
+                        return callback(null, url);
+                    });
+                });
+            }
+        }), 10, function (err) {
+            if (err) return console.error(err);
+            console.log('===> '+urls.count()+' images retrieved...');
+            callback();
         });
-      }
-    }), 10, function (err) {
-      if (err) return console.error(err);
-      console.log('Done! All images written to ./images');
     });
-  });
 }
+
+function retrieveResources(cursor)
+{
+    cursor = cursor || false;
+
+    var param = { type: 'upload', max_results: '500' };
+    if (cursor) param.next_cursor = cursor;
+
+    cloudinary.api.resources(function (result) {
+        var urls = result.resources.map(function (img) {
+            return img.url
+        });
+        downloadImages(urls, function(){
+            if (result.next_cursor) {
+                return retrieveResources(result.next_cursor);
+            }
+        });
+    }, param);
+}
+retrieveResources();
